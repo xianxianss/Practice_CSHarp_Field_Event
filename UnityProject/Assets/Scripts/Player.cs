3ecd83cceb8e1cd6b1256e5f0ce2c157e9db1d6a
xianxianss <75553503+xianxianss@users.noreply.github.com>
2020-12-30 00:27:55 +0800

    完成欄位與事件

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("移動速度"), Range(0, 100)]
+    public float Speed = 10f;
+    [Header("跳躍高度"), Range(0, 2000)]
+    public int Jump = 200;
+    [Header("對話內容")]
+    public string Talk = ("咕咕咕~~~");
+    [Header("是否取得權重")]
+    public bool Egg = false;
+
+    private void Start()
+    {
+        print("遊戲開始!!!");
+
+    }
+
+    private void Update()
+    {
+        print(Talk);
+    }
+
+
+
+
+
+
+
+}
